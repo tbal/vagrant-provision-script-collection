cfd5a2aa8560a38155f2abc1d46d949b634112d6
Tilo Baller <baller@cosmocode.de>
2014-12-15 21:46:49 +0100

    debian/common: Rename variable PACKAGES to COMMON_PACKAGES

--- debian/common.sh
+++ debian/common.sh
@@ -6,6 +6,6 @@ echo ">>> Setting Timezone to $TIMEZONE"
 echo "$TIMEZONE" | tee /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata
 
 
-PACKAGES="vim curl htop tree"
-echo ">>> Installing packages: $PACKAGES"
-apt-get install -qq $PACKAGES
+COMMON_PACKAGES="vim curl htop tree"
+echo ">>> Installing packages: $COMMON_PACKAGES"
+apt-get install -qq $COMMON_PACKAGES
