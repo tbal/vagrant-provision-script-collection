4b2ffcd0d1a37b52daeb7ea4464b84ec7b04383c
Tilo Baller <baller@cosmocode.de>
2014-12-15 21:48:50 +0100

    debian/common: Allow setting used variables via environment variables

--- debian/common.sh
+++ debian/common.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-TIMEZONE="Europe/Berlin"
+[ -z "$TIMEZONE" ] && TIMEZONE="Europe/Berlin"
 echo ">>> Setting Timezone to $TIMEZONE"
 echo "$TIMEZONE" | tee /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata
 
 
-COMMON_PACKAGES="vim curl htop tree"
+[ -z "$COMMON_PACKAGES" ] && COMMON_PACKAGES="vim curl htop tree"
 echo ">>> Installing packages: $COMMON_PACKAGES"
 apt-get install -qq $COMMON_PACKAGES
