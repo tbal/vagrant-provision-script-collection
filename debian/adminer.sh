1fd846fabf998e532755b6288e6957ff22086be9
Tilo Baller <baller@cosmocode.de>
2014-12-17 17:18:21 +0100

    debian/adminer: Add adminer installer script

---
+++ debian/adminer.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+[ -z "$ADMINER_VERSION"     ] && ADMINER_VERSION="4.1.0"
+[ -z "$ADMINER_INSTALL_DIR" ] && ADMINER_INSTALL_DIR="/var/www/adminer"
+[ -z "$ADMINER_SKIN_URL"    ] && ADMINER_SKIN_URL="https://raw.github.com/vrana/adminer/master/designs/pepa-linha/adminer.css"
+
+# remove trailing slash if set
+ADMINER_INSTALL_DIR="$(echo "$ADMINER_INSTALL_DIR" | sed 's/\/$//g')"
+
+
+echo ">>> Installing adminer $ADMINER_VERSION"
+
+# install curl temporary if missing
+CURL_UNINSTALL=0
+if [ ! $(which curl) ]; then
+    echo "curl required for installing adminer. Temporary installing curl ..."
+    apt-get install -qq curl > /dev/null 2>&1
+    CURL_UNINSTALL=1
+fi
+
+# download adminer and it's skin to target install directory
+mkdir -p "$ADMINER_INSTALL_DIR"
+curl -f -L -o "${ADMINER_INSTALL_DIR}/index.php" \
+     "http://downloads.sourceforge.net/adminer/adminer-${ADMINER_VERSION}.php"
+curl -f -L -o "${ADMINER_INSTALL_DIR}/adminer.css" "$ADMINER_SKIN_URL"
+echo "Installed adminer to ${ADMINER_INSTALL_DIR}/."
+
+# uninstall previously temporary installed curl
+if [ "$CURL_UNINSTALL" -eq 1 ]; then
+    apt-get purge -qq curl > /dev/null 2>&1
+    echo "Removed curl."
+fi
