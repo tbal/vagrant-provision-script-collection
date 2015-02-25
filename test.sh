85d98c3636c571b85437c4d167e986390a30c481
Tilo Baller <baller@cosmocode.de>
2015-02-25 18:06:55 +0100

    test: Add new parameter '-a' to run commands at the end of the test run

--- test.sh
+++ test.sh
@@ -6,7 +6,8 @@ CURRENT_DIR="$(readlink -f `dirname $0`)"
 [ -z "$DOCKER_CONTAINER"        ] && DOCKER_CONTAINER="debian:wheezy"
 [ -z "$DOCKER_MOUNT_HOST"       ] && DOCKER_MOUNT_HOST=$CURRENT_DIR
 [ -z "$DOCKER_MOUNT_GUEST"      ] && DOCKER_MOUNT_GUEST="/tmp/vpsc-test"
-[ -z "$DOCKER_INITIAL_CMD"      ] && DOCKER_INITIAL_CMD=
+[ -z "$DOCKER_CMD_BEFORE"       ] && DOCKER_CMD_BEFORE=
+[ -z "$DOCKER_CMD_AFTER"        ] && DOCKER_CMD_AFTER=
 [ -z "$DOCKER_UPDATE_APT_INDEX" ] && DOCKER_UPDATE_APT_INDEX=0
 
 
@@ -14,16 +15,19 @@ function show_help() {
     echo "Usage: $(basename "$0") [options] [<files>]
 
   -v  Show complete output (stdout and stderr) of the test runs.
-  -i  Set command which should be executed before any other files.
-      Example: ./$(basename "$0") -i 'cat /etc/debian_version && uname -a' [<files>]
+  -b  Set command which should be executed before any other files.
+      Example: ./$(basename "$0") -b 'cat /etc/debian_version && uname -a' [<files>]
+  -a  Set command which should be executed after any other files.
+      This e.g. allows you to start a shell and attach to the container.
+      Example: ./$(basename "$0") -a '/bin/bash' [<files>]
   -u  Runs 'apt-get update' before executing actual files.
-      Same as: ./$(basename "$0") -i 'apt-get update > /dev/null' [<files>]
+      Same as: ./$(basename "$0") -b 'apt-get update > /dev/null' [<files>]
   -h  Print this help."
 }
 
 
 OPTIND=1 # Reset is necessary if getopts was used previously in the script.  It is a good idea to make this local in a function.
-while getopts ":hvi:u" opt; do
+while getopts ":hvb:a:u" opt; do
     case "$opt" in
         h)
             show_help
@@ -32,8 +36,11 @@ while getopts ":hvi:u" opt; do
         v)
             VERBOSE=1
             ;;
-        i)
-            DOCKER_INITIAL_CMD=$OPTARG
+        b)
+            DOCKER_CMD_BEFORE=$OPTARG
+            ;;
+        a)
+            DOCKER_CMD_AFTER=$OPTARG
             ;;
         u)
             DOCKER_UPDATE_APT_INDEX=1
@@ -77,8 +84,11 @@ for SCRIPT in $SCRIPTS; do
 
     # prepare execute command
     DOCKER_EXECUTE_CMD="$DOCKER_MOUNT_GUEST/$SCRIPT"
-    if [ ! -z "$DOCKER_INITIAL_CMD" ]; then
-        DOCKER_EXECUTE_CMD="$DOCKER_INITIAL_CMD && $DOCKER_EXECUTE_CMD"
+    if [ ! -z "$DOCKER_CMD_BEFORE" ]; then
+        DOCKER_EXECUTE_CMD="$DOCKER_CMD_BEFORE && $DOCKER_EXECUTE_CMD"
+    fi
+    if [ ! -z "$DOCKER_CMD_AFTER" ]; then
+        DOCKER_EXECUTE_CMD="$DOCKER_EXECUTE_CMD && $DOCKER_CMD_AFTER"
     fi
     if [ "$DOCKER_UPDATE_APT_INDEX" -eq 1 ]; then
         DOCKER_EXECUTE_CMD="echo -n '>>> Updating apt index... ' && apt-get update > /dev/null && echo 'Done.' && $DOCKER_EXECUTE_CMD"
@@ -91,6 +101,7 @@ for SCRIPT in $SCRIPTS; do
 
         # actually run prepared command in docker container
         docker run \
+            -t -i \
             -v "$DOCKER_MOUNT_HOST":"$DOCKER_MOUNT_GUEST" \
             $DOCKER_CONTAINER \
             /usr/bin/env bash -c "$DOCKER_EXECUTE_CMD"
