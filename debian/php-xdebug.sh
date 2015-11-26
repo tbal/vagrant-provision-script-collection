4b00bb3a27220043aad8ad39273af35ee56f6e63
Tilo Baller <baller@cosmocode.de>
2015-11-26 14:26:38 +0100

    php-xdebug: Add configuration for remote debugging cli scripts

--- debian/php-xdebug.sh
+++ debian/php-xdebug.sh
@@ -3,6 +3,9 @@ set -e
 
 [ -z "$XDEBUG_CONFIG_FILE"   ] && XDEBUG_CONFIG_FILE="/etc/php5/conf.d/xdebug.ini"
 [ -z "$XDEBUG_CONFIG_PARAMS" ] && XDEBUG_CONFIG_PARAMS="xdebug.remote_enable=true xdebug.remote_connect_back=1 xdebug.max_nesting_level=500"
+[ -z "$XDEBUG_ENV_FILE"      ] && XDEBUG_ENV_FILE="/etc/profile.d/xdebug.sh"
+[ -z "$XDEBUG_ENV_PARAMS"    ] && XDEBUG_ENV_PARAMS="PHP_IDE_CONFIG=\"serverName=\${DOMAINS%% *}\" XDEBUG_CONFIG=\"remote_connect_back=0 remote_host=\$(netstat -rn | awk '{print \$2;}' | sed -n '3p') idekey=IDEA\""
+
 
 
 echo ">>> Installing xdebug"
@@ -14,7 +17,13 @@ mkdir -p $(dirname "$XDEBUG_CONFIG_FILE")
 touch $XDEBUG_CONFIG_FILE
 
 for XDEBUG_CONFIG_PARAM in $XDEBUG_CONFIG_PARAMS; do
-    grep -q "$XDEBUG_CONFIG_PARAM" $XDEBUG_CONFIG_FILE || echo "$XDEBUG_CONFIG_PARAM" >> $XDEBUG_CONFIG_FILE
+    grep -q "$XDEBUG_CONFIG_PARAM" $XDEBUG_CONFIG_FILE || echo "$XDEBUG_CONFIG_PARAM" >> "$XDEBUG_CONFIG_FILE"
 done
 
 echo $XDEBUG_CONFIG_PARAMS | sed 's/ /\n/g'
+
+
+echo ">>> Setting xdebug environment variables"
+echo "export $XDEBUG_ENV_PARAMS" > "$XDEBUG_ENV_FILE"
+
+echo $XDEBUG_ENV_PARAMS
