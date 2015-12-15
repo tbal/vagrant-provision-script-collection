38dfb07e935cb9915183e96cce58882d340f9d41
Tilo Baller <baller@cosmocode.de>
2015-12-15 17:46:31 +0100

    bundle/typo3-lamp: Allow skipping mysql and vhost setup

--- debian/bundle/typo3-lamp.sh
+++ debian/bundle/typo3-lamp.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+[ -z "$SKIP_DB_SETUP"      ] && SKIP_DB_SETUP=0
 [ -z "$TYPO3_DB_NAME"      ] && TYPO3_DB_NAME="typo3"
 [ -z "$TYPO3_DB_USER"      ] && TYPO3_DB_USER="typo3"
 [ -z "$TYPO3_DB_PASSWORD"  ] && TYPO3_DB_PASSWORD="typo3"
 [ -z "$APACHE_USER"        ] && APACHE_USER="vagrant"
 [ -z "$APACHE_GROUP"       ] && APACHE_GROUP="www-data"
+[ -z "$SKIP_VHOST_SETUP"   ] && SKIP_VHOST_SETUP=0
 [ -z "$PROJECT_VHOST_FILE" ] && PROJECT_VHOST_FILE="/vagrant/conf/httpd-vhost.vagrant.conf"
 [ -z "$PROJECT_VHOST_NAME" ] && PROJECT_VHOST_NAME="typo3"
 
@@ -28,19 +30,21 @@ source "$BASEDIR/../php-mysql.sh"
 ##
 
 # create mysql user and database
-echo ">>> Creating mysql database '$TYPO3_DB_NAME' and user '$TYPO3_DB_USER'"
-if [ -d "/var/lib/mysql/$TYPO3_DB_NAME" ]; then
-    echo "Database already exists. We expect that the user also already exists (without checking further)."
-else
-    MYSQL_QUERY="CREATE DATABASE \`$TYPO3_DB_NAME\` CHARACTER SET utf8; \
-        CREATE USER \`$TYPO3_DB_USER\`@'%' IDENTIFIED BY '$TYPO3_DB_PASSWORD'; \
-        GRANT USAGE ON *.* TO \`$TYPO3_DB_USER\`@'%' IDENTIFIED BY '$TYPO3_DB_PASSWORD'; \
-        GRANT ALL PRIVILEGES ON \`$TYPO3_DB_NAME\`.* TO \`$TYPO3_DB_USER\`@'%';"
-
-    if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-        mysql -uroot -e "$MYSQL_QUERY"
+if [ "$SKIP_DB_SETUP" -ne 1 ]; then
+    echo ">>> Creating mysql database '$TYPO3_DB_NAME' and user '$TYPO3_DB_USER'"
+    if [ -d "/var/lib/mysql/$TYPO3_DB_NAME" ]; then
+        echo "Database already exists. We expect that the user also already exists (without checking further)."
     else
-        mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "$MYSQL_QUERY"
+        MYSQL_QUERY="CREATE DATABASE \`$TYPO3_DB_NAME\` CHARACTER SET utf8; \
+            CREATE USER \`$TYPO3_DB_USER\`@'%' IDENTIFIED BY '$TYPO3_DB_PASSWORD'; \
+            GRANT USAGE ON *.* TO \`$TYPO3_DB_USER\`@'%' IDENTIFIED BY '$TYPO3_DB_PASSWORD'; \
+            GRANT ALL PRIVILEGES ON \`$TYPO3_DB_NAME\`.* TO \`$TYPO3_DB_USER\`@'%';"
+
+        if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+            mysql -uroot -e "$MYSQL_QUERY"
+        else
+            mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "$MYSQL_QUERY"
+        fi
     fi
 fi
 
@@ -57,10 +61,12 @@ apt-get install -qq $INSTALL_PHP_MODULES
 
 
 # link apache vhost
-echo ">>> Linking projects apache vhost config if it exists"
-VHOST_LINK_DESTINATION="/etc/apache2/sites-enabled/${PROJECT_VHOST_NAME}.conf"
-[ -f "$PROJECT_VHOST_FILE" ] && [ ! -f "$VHOST_LINK_DESTINATION" ] \
-    && ln -fs "$PROJECT_VHOST_FILE" "$VHOST_LINK_DESTINATION"
+if [ "$SKIP_VHOST_SETUP" -ne 1 ]; then
+    echo ">>> Linking projects apache vhost config if it exists"
+    VHOST_LINK_DESTINATION="/etc/apache2/sites-enabled/${PROJECT_VHOST_NAME}.conf"
+    [ -f "$PROJECT_VHOST_FILE" ] && [ ! -f "$VHOST_LINK_DESTINATION" ] \
+        && ln -fs "$PROJECT_VHOST_FILE" "$VHOST_LINK_DESTINATION"
+fi
 
 
 # stop apache
