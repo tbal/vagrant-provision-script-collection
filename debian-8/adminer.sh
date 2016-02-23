c991cc506d94d6252f97562460d1476094666149
Tilo Baller <baller@cosmocode.de>
2016-02-23 14:00:08 +0100

    adminer: Fix installation path for debian-8 installations

--- debian-8/adminer.sh
+++ debian-8/adminer.sh
@@ -1,1 +1,9 @@
-../debian-7/adminer.sh
+#!/usr/bin/env bash
+set -e
+
+[ -z "$ADMINER_INSTALL_DIR" ] && ADMINER_INSTALL_DIR="/var/www/html/adminer"
+
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$BASEDIR/../debian-7/adminer.sh"
