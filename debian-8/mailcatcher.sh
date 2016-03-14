e54df67e90a4692083c349be0c346ceca45f5fc8
Tilo Baller <baller@cosmocode.de>
2016-03-14 15:34:20 +0100

    mailcatcher: Fix php settings were not applied on debian 8

--- debian-8/mailcatcher.sh
+++ debian-8/mailcatcher.sh
@@ -1,1 +1,13 @@
-../debian-7/mailcatcher.sh
+#!/usr/bin/env bash
+set -e
+
+[ -z "$MAILCATCHER_PHP_CONFIG_FILE"  ] && MAILCATCHER_PHP_CONFIG_FILE="/etc/php5/mods-available/mailcatcher.ini"
+
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$BASEDIR/../debian-7/mailcatcher.sh"
+
+
+echo ">>> Enabling mailcatcher php settings"
+php5enmod mailcatcher
