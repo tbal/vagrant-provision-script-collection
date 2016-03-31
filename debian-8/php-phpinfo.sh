25335a29a295fb3cb09fd3b7cdad49b3009838ef
Tilo Baller <baller@cosmocode.de>
2016-03-31 16:10:03 +0200

    php-phpinfo: Fix installation directory in debian jessie and ubuntu

--- debian-8/php-phpinfo.sh
+++ debian-8/php-phpinfo.sh
@@ -1,1 +1,9 @@
-../debian-7/php-phpinfo.sh
+#!/usr/bin/env bash
+set -e
+
+[ -z "$PHPINFO_DESTINATION_DIR" ] && PHPINFO_DESTINATION_DIR="/var/www/html"
+
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$BASEDIR/../debian-7/php-phpinfo.sh"
