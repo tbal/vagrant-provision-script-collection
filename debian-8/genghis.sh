309f94ae569974b3c672df1354f09d955a29c42d
Tilo Baller <baller@cosmocode.de>
2016-03-31 16:09:43 +0200

    genghis: Fix installation directory in debian jessie and ubuntu

--- debian-8/genghis.sh
+++ debian-8/genghis.sh
@@ -1,1 +1,9 @@
-../debian-7/genghis.sh
+#!/usr/bin/env bash
+set -e
+
+[ -z "$GENGHIS_INSTALL_PATH" ] && GENGHIS_INSTALL_PATH="/var/www/html/genghis.php"
+
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$BASEDIR/../debian-7/genghis.sh"
