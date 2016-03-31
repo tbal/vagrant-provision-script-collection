d6db8fd3ec00525c145cea24c9030a39ad22f7c3
Tilo Baller <baller@cosmocode.de>
2016-03-31 16:10:30 +0200

    php-xdebug: Fix installation path for debian-8 and ubuntu installations

--- debian-8/php-xdebug.sh
+++ debian-8/php-xdebug.sh
@@ -1,1 +1,13 @@
-../debian-7/php-xdebug.sh
+#!/usr/bin/env bash
+set -e
+
+[ -z "$XDEBUG_CONFIG_FILE"  ] && XDEBUG_CONFIG_FILE="/etc/php5/mods-available/xdebug-vpsc.ini"
+
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$BASEDIR/../debian-7/php-xdebug.sh"
+
+
+echo ">>> Enabling xdebug php settings"
+php5enmod xdebug-vpsc
