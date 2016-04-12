fd6cb7ecda505992808114d843d1a5f7fe37652a
Tilo Baller <baller@cosmocode.de>
2016-04-12 16:44:24 +0200

    solr-tomcat: Use tomcat 8 for debian 8

--- debian-8/solr-tomcat.sh
+++ debian-8/solr-tomcat.sh
@@ -1,1 +1,9 @@
-../debian-7/solr-tomcat.sh
+#!/usr/bin/env bash
+set -e
+
+[ -z "$TOMCAT_VERSION" ] && TOMCAT_VERSION=8
+
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source "$BASEDIR/../debian-7/solr-tomcat.sh"
